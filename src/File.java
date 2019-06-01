2af034cbb664a4897bd8eeb9f078860b2ecc5f0f
tal_malul <tal.malul4@gmail.com>
2019-06-01 23:28:18 +0300

    added file Class

---
+++ src/File.java
@@ -0,0 +1,23 @@
+public class File {
+    private int Id;
+    private int requiredSize;
+    private int downloaded;
+
+    public File(int id, int requiredSize, int downloaded) {
+        Id = id;
+        this.requiredSize = requiredSize;
+        this.downloaded = downloaded;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public int getRequiredSize() {
+        return requiredSize;
+    }
+
+    public int getDownloaded() {
+        return downloaded;
+    }
+}
