8549c551e25936274417754ef11a6804e321cfa9
tal_malul <tal.malul4@gmail.com>
2019-06-01 23:32:07 +0300

    added queue<file> to movie streamer

--- src/MovieStreamer.java
+++ src/MovieStreamer.java
@@ -1,15 +1,21 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class MovieStreamer {
 
 
-private MovieStreamerState onState;
-private MovieStreamerState offState;
 
-private MovieStreamerState currentState;
+    private Queue<File> fileReuqestQueue;
+    private MovieStreamerState onState;
+    private MovieStreamerState offState;
+
+    private MovieStreamerState currentState;
 
 
     public MovieStreamer(){
         onState = new OnState(this);
         offState = new OffState(this);
+        fileReuqestQueue = new PriorityQueue<>();
     }
 
     public MovieStreamerState getOnState() {
