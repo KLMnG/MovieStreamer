6cf898ec7859488e80584bebd65b039ba11bee05
tal_malul <tal.malul4@gmail.com>
2019-06-02 00:49:56 +0300

    added states

--- src/MovieStreamer.java
+++ src/MovieStreamer.java
@@ -9,6 +9,34 @@ public class MovieStreamer {
     private MovieStreamerState onState;
     private MovieStreamerState offState;
 
+
+
+    //OnState
+    private MovieStreamerState internetConnectionRegion;
+    private MovieStreamerState connectionStatusRegion;
+
+    //InternetConnection
+    private MovieStreamerState internetConnectedState;
+    private MovieStreamerState internetDisconnectedState;
+    private MovieStreamerState downloadRegionState;
+
+    //DownloadingRegion
+    private MovieStreamerState downloadState;
+    private MovieStreamerState secondAttemptState;
+    private MovieStreamerState idleState;
+    private MovieStreamerState dErrorState;
+    private MovieStreamerState verifyRequirementState;
+
+    //DeviceManagerRegion
+    private MovieStreamerState deviceManagerState;
+
+    //MovieStreamerRegion
+    private MovieStreamerState idleStreamingState;
+    private MovieStreamerState pauseState;
+    private MovieStreamerState streamingState;
+
+    private System system;
+
     private MovieStreamerState currentState;
     private File currentDownloadFile;
 
@@ -16,7 +44,10 @@ public class MovieStreamer {
         onState = new OnState(this);
         offState = new OffState(this);
         fileReuqestQueue = new PriorityQueue<>();
+        system = new System(false,100);
     }
+
+
 
     public MovieStreamerState getOnState() {
         return onState;
@@ -24,6 +55,66 @@ public class MovieStreamer {
 
     public MovieStreamerState getOffState() {
         return offState;
+    }
+
+    public MovieStreamerState getInternetConnectionRegion() {
+        return internetConnectionRegion;
+    }
+
+    public MovieStreamerState getConnectionStatusRegion() {
+        return connectionStatusRegion;
+    }
+
+    public MovieStreamerState getInternetConnectedState() {
+        return internetConnectedState;
+    }
+
+    public MovieStreamerState getInternetDisconnectedState() {
+        return internetDisconnectedState;
+    }
+
+    public MovieStreamerState getDownloadRegionState() {
+        return downloadRegionState;
+    }
+
+    public MovieStreamerState getDownloadState() {
+        return downloadState;
+    }
+
+    public MovieStreamerState getSecondAttemptState() {
+        return secondAttemptState;
+    }
+
+    public MovieStreamerState getIdleState() {
+        return idleState;
+    }
+
+    public MovieStreamerState getdErrorState() {
+        return dErrorState;
+    }
+
+    public MovieStreamerState getVerifyRequirementState() {
+        return verifyRequirementState;
+    }
+
+    public MovieStreamerState getDeviceManagerState() {
+        return deviceManagerState;
+    }
+
+    public MovieStreamerState getIdleStreamingState() {
+        return idleStreamingState;
+    }
+
+    public MovieStreamerState getPauseState() {
+        return pauseState;
+    }
+
+    public MovieStreamerState getStreamingState() {
+        return streamingState;
+    }
+
+    public MovieStreamerState getCurrentState() {
+        return currentState;
     }
 
     public void setState(MovieStreamerState state){
@@ -34,7 +125,15 @@ public class MovieStreamer {
         return fileReuqestQueue;
     }
 
+    public void setCurrentDownloadFile(File f) {
+        currentDownloadFile = f;
+    }
+
     public File getCurrentDownloadFile() {
         return currentDownloadFile;
+    }
+
+    public System getSystem() {
+        return system;
     }
 }
