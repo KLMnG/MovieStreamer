69ec981c9718fbc3936e92aa540a1eeff95bcfa3
tal_malul <tal.malul4@gmail.com>
2019-06-01 11:57:57 +0300

    Tals commit

---
+++ src/MovieStreamerState.java
@@ -0,0 +1,20 @@
+public interface MovieStreamerState {
+
+    public void movieOn();
+    public void doneDownload();
+    public void holdMovie();
+    public void downloadError();
+    public void restart();
+    public void downloadAborted();
+    public void resume();
+    public void errorFixed();
+    public void moviePending();
+    public void internetOff();
+    public void internetOn();
+    public void fileRequest();
+    public void removed();
+    public void turnOn();
+    public void turnOff();
+
+
+}
---
+++ src/VerifyRequirementsState.java
@@ -0,0 +1,2 @@
+public class VerifyRequirementsState {
+}
